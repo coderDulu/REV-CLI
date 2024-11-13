dc683c5f5441df160da6758e8ad77980c6011362
jf <1791105501@qq.com>
2024-11-13 14:30:40 +0800

    连接添加加载中

--- src/renderer/src/components/ConnectDialog.tsx
+++ src/renderer/src/components/ConnectDialog.tsx
@@ -1,126 +1,153 @@
+import { Modal, Button, Form, Space, InputNumber } from "antd"
+import type { FormProps } from "antd"
+import IpInput from "./IpInput"
 
-import { Modal, Button, Form, Space, InputNumber } from 'antd';
-import type { FormProps } from 'antd';
-import IpInput from './IpInput';
-
-import { useConnect, useConnectDispatch } from '@/hooks/useConnect';
-import { useListenMainEvent } from '@/hooks/useMainEvent';
-import { useCallback } from 'react';
+import { useConnect, useConnectDispatch } from "@/hooks/useConnect"
+import { useListenMainEvent } from "@/hooks/useMainEvent"
+import { useCallback, useEffect, useRef, useState } from "react"
 type FieldType = {
-  address: string;
-  port: number;
-};
+  address: string
+  port: number
+}
 
 interface Props {
   showModel: boolean
   onHide: () => void
 }
 
-
 function ConnectForm({ showModel, onHide }: Props) {
   const connect = useConnect()
   const connectDispatch = useConnectDispatch()
+  const [isConnecting, setIsConnecting] = useState(false)
+  const connectCacheRef = useRef(connect.isConnect)
+
+  useEffect(() => {
+    // connected => disconnected reload page
+    if (connectCacheRef.current === true && connect.isConnect === false) {
+      location.reload()
+    }
+    return () => {
+      connectCacheRef.current = connect.isConnect
+    }
+  }, [connect.isConnect])
 
   const subscribe = useCallback((e, isClosed: boolean) => {
     if (isClosed) {
       connectDispatch({
         type: "update",
-        isConnect: false
+        isConnect: false,
       })
-      location.reload()
+      setIsConnecting(false)
     }
   }, [])
 
-  useListenMainEvent('ws-closed', subscribe)
+  useListenMainEvent("ws-closed", subscribe)
 
-
-  const onFinished: FormProps<FieldType>['onFinish'] = async (values) => {
+  const onFinished: FormProps<FieldType>["onFinish"] = async (values) => {
     try {
-      const role = await window.electron.invoke('ws-connect', { address: values.address, port: values.port })
-
+      setIsConnecting(true)
       connectDispatch({
         type: "update",
         address: values.address,
         port: values.port,
+      })
+
+      const role = await window.electron.invoke("ws-connect", {
+        address: values.address,
+        port: values.port,
+      })
+
+      connectDispatch({
+        type: "update",
         isConnect: true,
-        role
+        role,
       })
       onHide()
       window.$message.success("连接成功")
+      setIsConnecting(false)
     } catch (error) {
       handleWsClose()
     }
-  };
+  }
 
   function handleWsClose() {
     window.$message.error("连接错误")
     connectDispatch({
       type: "update",
-      isConnect: false
+      isConnect: false,
     })
   }
 
-  const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-  };
+  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
+    console.log("Failed:", errorInfo)
+  }
 
   async function handleDisconnect() {
     try {
-      await window.electron.invoke('ws-disconnect')
+      await window.electron.invoke("ws-disconnect")
 
       connectDispatch({
         type: "update",
-        isConnect: false
+        isConnect: false,
       })
       window.$message.success("断开连接成功")
+      setIsConnecting(false)
     } catch (error) {
-      console.log('disconnect', error);
+      console.log("disconnect", error)
     }
   }
 
-  return <Modal title="设备连接" footer={null} centered maskClosable={false} open={showModel} onCancel={onHide}>
-    <Form
-      name="basic"
-      labelCol={{ span: 6 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
-      initialValues={{ address: connect.address, port: connect.port }}
-
-      onFinish={onFinished}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-
+  return (
+    <Modal
+      title="设备连接"
+      footer={null}
+      centered
+      maskClosable={false}
+      open={showModel}
+      onCancel={onHide}
     >
-      <Form.Item<FieldType>
-        label="IP地址"
-        name="address"
+      <Form
+        name="basic"
+        labelCol={{ span: 6 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ address: connect.address, port: connect.port }}
+        onFinish={onFinished}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <IpInput address={connect.address || ""} />
-      </Form.Item>
+        <Form.Item<FieldType> label="IP地址" name="address">
+          <IpInput address={connect.address || ""} />
+        </Form.Item>
 
-      <Form.Item<FieldType>
-        label="port"
-        name="port"
-        rules={[{ required: true, message: '请输入' }]}
-      >
-        <InputNumber min={0} max={65535} value={connect.port} />
-      </Form.Item>
+        <Form.Item<FieldType>
+          label="port"
+          name="port"
+          rules={[{ required: true, message: "请输入" }]}
+        >
+          <InputNumber min={0} max={65535} value={connect.port} />
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 10, span: 16 }} className='m-4'>
-        <Space>
-          <Button type="default" onClick={onHide}>
-            取消
-          </Button>
-          <Button danger onClick={handleDisconnect} >
-            断开连接
-          </Button>
-          <button className='bg-[#0d8383] w-20 h-8 rounded text-white' type="submit">
-            连接
-          </button>
-        </Space>
-      </Form.Item>
-    </Form>
-  </Modal>
+        <Form.Item wrapperCol={{ offset: 10, span: 16 }} className="m-4">
+          <Space>
+            <Button type="default" onClick={onHide}>
+              取消
+            </Button>
+            <Button danger onClick={handleDisconnect}>
+              断开连接
+            </Button>
+            <Button
+              loading={isConnecting}
+              className="bg-[#0d8383] w-20 h-8 rounded text-white"
+              htmlType="submit"
+            >
+              连接
+            </Button>
+          </Space>
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
 }
 
-export default ConnectForm;
+export default ConnectForm
